2220b7a5833340c1fab55f6fce8bcb4681aa0af3
unknown <evasquezcam@gmail.com>
2021-03-09 00:59:00 -0400

    changed

--- app grafica1/Form.cs
+++ app grafica1/Form.cs
@@ -16,6 +16,9 @@ namespace app_grafica1
         public double digito1 = 0;
         public double digito2 = 0;
         public string operacionARealizar = "/";
+        public bool fueCalculado = false;
+        public double resultadoTotal = 0;
+
         public Form()
         {
             InitializeComponent();
@@ -57,15 +60,18 @@ namespace app_grafica1
         private void btnBorrar_Click(object sender, EventArgs e)
         {
             //Sterminar
-            if (textBox.Text.Length > 0) { 
-            var retorno = textBox.Text.Substring(0, textBox.Text.Length-1);
-            textBox.Text = retorno;
+            if (textBox.Text.Length > 0)
+            {
+                var retorno = textBox.Text.Substring(0, textBox.Text.Length - 1);
+                textBox.Text = retorno;
+
+                fueCalculado = true;
             }
         }
 
         private void btnIgual_Click(object sender, EventArgs e)
-        {            
-             calcular();
+        {
+          calcular();
             
         }
 
@@ -81,86 +87,171 @@ namespace app_grafica1
 
         private void btn0_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
+            
             textBox.Text += (sender as Button).Text;
+
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         private void bnt7_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
+            if (fueCalculado)
+            {
+                textBox.Text = "";
+                fueCalculado = false;
+            }
             textBox.Text += (sender as Button).Text;
         }
 
         public void  calcular()
         {
-            var split = textBox.Text.Split(operacionARealizar);
-            if (digito1 >= 0 && digito2 >= 0)
-            { 
-            digito1 = double.Parse(split[0]);
-            digito2 = double.Parse(split[1]);
-
-            }
-
-            Calcular iniciarCalcular = new Calcular();
-            switch (operacionARealizar)
+            try
             {
-                case "+":
-                    var retornoSum =iniciarCalcular.sumar(digito1, digito2);
-                    textBox.Text = retornoSum.ToString();
-                    break;
-                case "-":
-                   var retornoRes = iniciarCalcular.restar(digito1, digito2);
-                    textBox.Text = retornoRes.ToString();
-                    break;
-                case "*":
-                   var retornoMultip= iniciarCalcular.multiplicar(digito1, digito2);
-                    textBox.Text = retornoMultip.ToString();
-                    break;
-                case "/":
-                   var retornoDiv = iniciarCalcular.dividir(digito1, digito2);
-                    textBox.Text = retornoDiv.ToString();
-                    break;
-            }
-               
-        }
 
+                switch (textBox.Text.Substring(0, 1))
+                {
+                    case "+":
+                        MessageBox.Show("No puedes usar un operardor sin antes especificar un numero", "Error");
+                        textBox.Text = "0";
+                        break;
+                    case "-":
+                        MessageBox.Show("No puedes usar un operardor sin antes especificar un numero", "Error");
+                        textBox.Text = "0";
+                        break;
+                    case "*":
+                        MessageBox.Show("No puedes usar un operardor sin antes especificar un numero", "Error");
+                        textBox.Text = "0";
+                        break;
+                    case "/":
+                        MessageBox.Show("No puedes usar un operardor sin antes especificar un numero", "Error");
+                        textBox.Text = "0";
+                        break;
+                    default:
+                        var split = textBox.Text.Split(operacionARealizar);
+                        if (digito1 >= 0 && digito2 >= 0)
+                        {
+                            digito1 = double.Parse(split[0]);
+                            digito2 = double.Parse(split[1]);
+
+                        }
+
+                        Calcular iniciarCalcular = new Calcular();
+                        switch (operacionARealizar)
+                        {
+                            case "+":
+                                var retornoSum = iniciarCalcular.sumar(digito1, digito2);
+                                textBox.Text = retornoSum.ToString();
+                                break;
+                            case "-":
+                                var retornoRes = iniciarCalcular.restar(digito1, digito2);
+                                textBox.Text = retornoRes.ToString();
+                                break;
+                            case "*":
+                                var retornoMultip = iniciarCalcular.multiplicar(digito1, digito2);
+                                textBox.Text = retornoMultip.ToString();
+                                break;
+                            case "/":
+                                var retornoDiv = iniciarCalcular.dividir(digito1, digito2);
+                                textBox.Text = retornoDiv.ToString();
+                                break;
+                        }
+                        break;
+                }
+                fueCalculado = true;
+
+
+            }
+
+
+            catch
+            {
+                MessageBox.Show("No puedes usar digitos que no sean  numeros", "Error");
+                textBox.Text = "0";
+            }
+        }
     }
 }
